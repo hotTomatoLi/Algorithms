1c7ac4b6ff9b2c8a0fad85032fcf4b384e4c0556
lienquan <li.enquan@163.com>
2016-12-07 08:50:28 +0800

    希尔算法

--- src/main/java/com/lee/algorithms/sort/insert/ShellSort.java
+++ src/main/java/com/lee/algorithms/sort/insert/ShellSort.java
@@ -3,7 +3,20 @@ package com.lee.algorithms.sort.insert;
 import com.lee.algorithms.sort.BasicSort;
 
 /**
- * 插入排序算法
+ * 希尔排序
+ * 希尔排序的思想：
+ * 1.设定一个间隔gap，每次对gap间隔的子数字进行直接插入排序；
+ * 2.不断递减gap，直到gap=1
+ * 3.gap=1时相当于直接插入排序
+ * 例如
+ * a0,a1,a2,a3,a4,a5,a5,a7,a7,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20
+ * 当gap==5时，会进行如下分组
+ * a0,a5,a10,a15,a20
+ * a1,a6,a11,a16
+ * a2,a7,a12,a17
+ * a3,a8,a13,a18
+ * a4,a9,a14,a19
+ * 分别进行直接插入排序
  * @author lee
  * @since 1.0.0
  */
@@ -20,12 +33,12 @@ public class ShellSort extends BasicSort {
             int gap = this.data.length / 2;
             while(1 <= gap){
                 System.out.println("gap=" + gap);
-                for( int i = 0; i + gap <this.data.length; i++){
-                    int temp = this.data[i+gap];
-                    for(int j = i + gap -1; j >= i; j--){
-                        printData();
-                        if(temp < this.data[j]){
-                            swap(this.data,i+gap,j);
+                for(int i = gap; i < this.data.length; i=i+gap ){
+                    int temp = this.data[i];
+                    for(int j = i - gap; j >=0; j = j-gap){
+                        if(this.data[j] > temp){
+                            swap(this.data,j,j+gap);
+                            printData();
                         }else{
                             break;
                         }
